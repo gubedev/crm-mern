a6143bf7b32242411c107d25e468137a0e7aa0b1
Gustavo Benitez <gustavo.benitez@biting.com.ar>
2019-09-24 00:41:10 -0300

    front end & back end folder creation

---
+++ back-end/src/controllers/welcome.controller.js
@@ -0,0 +1,29 @@
+/**
+ *
+ *
+ * @class WelcomeController
+ */
+class WelcomeController {
+  /**
+   * greeting
+   *
+   * @static
+   * @param {object} req express request object
+   * @param {object} res express response object
+   * @param {object} next next middleware
+   * @returns {json} json object with status and gretting data
+   * @memberof WelcomeController
+   */
+    static async greeting(req, res, next) {
+        try {
+            return res.status(200).json({
+                status: true,
+                message: 'Welcome to api rest crm !!',
+            });
+        } catch (error) {
+            return next(error);
+        }
+    }
+}
+
+export default WelcomeController;
